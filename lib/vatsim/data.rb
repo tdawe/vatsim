b6fa5a386afb2bf66e27c1917986f0750a6b0075
tdawe <trevor.dawe@gmail.com>
2012-04-26 20:58:19 -0300

    Set static variable for previously parsed client data

--- lib/vatsim/data.rb
+++ lib/vatsim/data.rb
@@ -1,26 +1,29 @@
 module Vatsim
   class Data
+    
+    @@clients = Array.new
+    
     def self.parse_clients
 
-      Vatsim::Status.download
-      Vatsim::Status.parse
-            
-      clients = Array.new
-
-      parsing = false
-
-      File.open(Vatsim::Status::VATSIMDATA_FILE_PATH, 'r:ascii-8bit').each { |line|
-
-        parsing = false if parsing and line.start_with? ";"
-
-        if parsing
-          clients << Client.new(line)
-        end
-        
-        parsing = true if line.start_with? "!CLIENTS:"
-      }
+      if @@clients.length == 0
+        Vatsim::Status.download
+        Vatsim::Status.parse
+              
+        parsing = false
+  
+        File.open(Vatsim::Status::VATSIMDATA_FILE_PATH, 'r:ascii-8bit').each { |line|
+  
+          parsing = false if parsing and line.start_with? ";"
+  
+          if parsing
+            @@clients << Client.new(line)
+          end
+          
+          parsing = true if line.start_with? "!CLIENTS:"
+        }
+      end
       
-      return clients
+      return @@clients
       
     end
   end
