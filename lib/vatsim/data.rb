d9891c65ae53d8cd7292990f841b7fbd38c64bcf
tdawe <trevor.dawe@gmail.com>
2012-04-26 20:33:08 -0300

    Set parsing to true at end so that we don't end up parsing the CLIENTS line

--- lib/vatsim/data.rb
+++ lib/vatsim/data.rb
@@ -8,12 +8,13 @@ module Vatsim
 
       File.open(Vatsim::Status::VATSIMDATA_FILE_PATH, 'r:ascii-8bit').each { |line|
 
-        parsing = true if line.start_with? "!CLIENTS:"
         parsing = false if parsing and line.start_with? ";"
 
         if parsing
           clients << Client.new(line)
         end
+        
+        parsing = true if line.start_with? "!CLIENTS:"
       }
       
       return clients
