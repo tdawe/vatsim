f4916f1a7924180b672f5c31a6fa0231a4a9e75a
Trevor Dawe <trevor.dawe@gmail.com>
2012-04-27 17:04:26 -0300

    Added TODO comment for splitting the line

--- lib/vatsim/client.rb
+++ lib/vatsim/client.rb
@@ -30,6 +30,7 @@ module Vatsim
       
       attributes = [:callsign, :cid, :realname, :clienttype, :frequency, :latitude, :longitude, :altitude, :groundspeed, :planned_aircraft, :planned_tascruise, :planned_depairport, :planned_altitude, :planned_destairport, :server, :protrevision, :rating, :transponder, :facilitytype, :visualrange, :planned_revision, :planned_flighttype, :planned_deptime, :planned_actdeptime, :planned_hrsenroute, :planned_minenroute, :planned_hrsfuel, :planned_minfuel, :planned_altairport, :planned_remarks, :planned_route, :planned_depairport_lat, :planned_depairport_lon, :planned_destairport_lat, :planned_destairport_lon, :atis_message, :time_last_atis_received, :time_logon, :heading, :QNH_iHg, :QNH_Mb]
       
+      #TODO split line into an array here so that it doesn't have to be split so many times
       attributes.each_with_index.map { |attribute, index|
         instance_variable_set("@#{attribute}", line.split(":")[index]) if self.respond_to?(attribute)
       }
