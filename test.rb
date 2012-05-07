a4e00a0f06b69331b30dea35cab889423a6c17f9
tdawe <trevor.dawe@gmail.com>
2012-05-07 20:58:45 -0300

    Updated sample test script

--- test.rb
+++ test.rb
@@ -1,11 +1,14 @@
 require './lib/vatsim.rb'
 
-puts "There are #{Vatsim::Client.all.length} connected clients"
-puts "There are #{Vatsim::Client.pilots.length} connected pilots"
-puts "There are #{Vatsim::Client.atc.length} connected air traffic controllers"
-puts "There are #{Vatsim::Prefile.all.length} prefiled pilots"
+data = nil
+#data = Vatsim::Data.new("download_files" => false, "data_file_path" => File.dirname(__FILE__) + "/spec/vatsim-data.txt")
 
-Vatsim::General.all.each { |p,a|
+puts "There are #{Vatsim::Client.all(data).length} connected clients"
+puts "There are #{Vatsim::Client.pilots(data).length} connected pilots"
+puts "There are #{Vatsim::Client.atc(data).length} connected air traffic controllers"
+puts "There are #{Vatsim::Prefile.all(data).length} prefiled pilots"
+
+Vatsim::General.all(data).each { |p,a|
   puts "#{p} = #{a}"
 }
 
