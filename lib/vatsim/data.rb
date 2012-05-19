695fc4a37b0465cd3e24ed1767933c697c3c2520
tdawe <trevor.dawe@gmail.com>
2012-05-19 20:24:19 -0300

    Fixed whitespace

--- lib/vatsim/data.rb
+++ lib/vatsim/data.rb
@@ -28,7 +28,6 @@ module Vatsim
       end
 
       parse
-
     end
 
     # Returns all connected Pilots
@@ -53,36 +52,35 @@ module Vatsim
 
     # Parse the vatsim data file and store output as necessary
     def parse
-	      download_files if @refresh_files
+      download_files if @refresh_files
 
-        parsing_clients = false
-        parsing_prefile = false
-        parsing_general = false
+      parsing_clients = false
+      parsing_prefile = false
+      parsing_general = false
 
-	      File.open(@data_file_path, 'r:ascii-8bit').each { |line|
+      File.open(@data_file_path, 'r:ascii-8bit').each { |line|
 
-          parsing_clients = false if line.start_with? ";"
-          parsing_prefile = false if line.start_with? ";"
-          parsing_general = false if line.start_with? ";"
+        parsing_clients = false if line.start_with? ";"
+        parsing_prefile = false if line.start_with? ";"
+        parsing_general = false if line.start_with? ";"
 
-          if parsing_clients
-            @clients << Client.new(line)
-          elsif parsing_prefile
-            @prefiles << Prefile.new(line)
-          elsif parsing_general
-            line_split = line.split("=")
-            @general[line_split[0].strip.downcase.gsub(" ", "_")] = line_split[1].strip
-          end
+        if parsing_clients
+          @clients << Client.new(line)
+        elsif parsing_prefile
+          @prefiles << Prefile.new(line)
+        elsif parsing_general
+          line_split = line.split("=")
+          @general[line_split[0].strip.downcase.gsub(" ", "_")] = line_split[1].strip
+        end
 
-          parsing_clients = true if line.start_with? "!CLIENTS:"
-          parsing_prefile = true if line.start_with? "!PREFILE:"
-          parsing_general = true if line.start_with? "!GENERAL:"
-        }
+        parsing_clients = true if line.start_with? "!CLIENTS:"
+        parsing_prefile = true if line.start_with? "!PREFILE:"
+        parsing_general = true if line.start_with? "!GENERAL:"
+      }
     end
 
     # Initialize the system by downloading status and vatsim data files
     def download_files
-
       if !File.exists?(@status_file_path) or File.mtime(@status_file_path) < Time.now - STATUS_DOWNLOAD_INTERVAL
        download_to_file STATUS_URL, @status_file_path
       end
@@ -90,7 +88,6 @@ module Vatsim
       if !File.exists?(@data_file_path ) or File.mtime(@data_file_path ) < Time.now - DATA_DOWNLOAD_INTERVAL
         download_to_file random_data_url, @data_file_path
       end
-
     end
 
     # Download a url to a file path
@@ -99,18 +96,16 @@ module Vatsim
 
       File.new(file, File::CREAT)
 
-	    Net::HTTP.start(url.host) { |http|
+      Net::HTTP.start(url.host) { |http|
         resp = http.get(url.path)
         open(file, "wb") { |file|
           file.write(resp.body)
         }
       }
-
     end
 
     # Return random vatsim data url from status file
     def random_data_url
-
       url0s = Array.new
       file = File.open(@status_file_path)
       file.each {|line|
@@ -119,7 +114,6 @@ module Vatsim
         end
       }
       return url0s[rand(url0s.length)]
-
      end
   end
 end
