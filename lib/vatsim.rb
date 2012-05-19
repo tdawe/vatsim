4948c1e336c414decce784f13fe90c7f64fab8e3
tdawe <trevor.dawe@gmail.com>
2012-05-19 20:18:51 -0300

    Deleted unused General class

--- lib/vatsim.rb
+++ lib/vatsim.rb
@@ -1,6 +1,5 @@
 require 'vatsim/data'
 require 'vatsim/client'
 require 'vatsim/prefile'
-require 'vatsim/general'
 require 'vatsim/version'
 
--- lib/vatsim/general.rb
+++
@@ -1,6 +0,0 @@
-module Vatsim
-  # General properties from vatsim data file
-  class General
-  end
-end
-
