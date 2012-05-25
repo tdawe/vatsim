f03af11bb9d3b4f66cf11a8bf8204687a59aee5a
tdawe <trevor.dawe@gmail.com>
2012-05-24 21:25:54 -0300

    Pilot and ATC only use subset of all available parameters because not all are set for each Client type

--- lib/vatsim/atc.rb
+++ lib/vatsim/atc.rb
@@ -1,6 +1,7 @@
 module Vatsim
   # Connected pilot
   class ATC < Client
+    attr_reader :callsign, :cid, :realname, :clienttype, :frequency, :latitude, :longitude, :server, :protrevision, :rating, :facilitytype, :visualrange, :time_logon
   end
 end
 
--- lib/vatsim/pilot.rb
+++ lib/vatsim/pilot.rb
@@ -1,6 +1,7 @@
 module Vatsim
   # Connected pilot
   class Pilot < Client
+    attr_reader :callsign, :cid, :realname, :clienttype, :latitude, :longitude, :altitude, :groundspeed, :planned_aircraft, :planned_tascruise, :planned_depairport, :planned_altitude, :planned_destairport, :server, :protrevision, :rating, :transponder, :planned_revision, :planned_flighttype, :planned_deptime, :planned_actdeptime, :planned_hrsenroute, :planned_minenroute, :planned_hrsfuel, :planned_minfuel, :planned_altairport, :planned_remarks, :planned_route, :planned_depairport_lat, :planned_depairport_lon, :planned_destairport_lat, :planned_destairport_lon, :time_logon, :heading, :QNH_iHg, :QNH_Mb
   end
 end
 
