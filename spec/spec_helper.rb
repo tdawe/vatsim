871640c7e3d2df3ba8143dbd2c1ffb0a3b244cb9
Trevor Dawe <trevor.dawe@gmail.com>
2018-03-03 14:57:56 -0400

    Removed deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,8 @@ require 'vatsim'
 require 'webmock/rspec'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.expect_with(:rspec) { |c| c.syntax = :should }
 end
 
